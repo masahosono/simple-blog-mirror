18a63df373883430f87296e86bcc0d7451d086d4
masahosono <dev.masahosono@gmail.com>
2020-04-12 13:26:12 +0900

    add server.ts

---
+++ server.ts
@@ -0,0 +1,22 @@
+// ./server.js
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handler = app.getRequestHandler()
+const server = express()
+
+app.prepare().then(() => {
+    server.get('*', (req, res) => {
+        return handler(req, res)
+    })
+
+    server.listen(3000, err => {
+        if (err) console.error(err.stack)
+        console.debug('> Ready on http://localhost:3000')
+    })
+}).catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+})
