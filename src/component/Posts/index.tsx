516835c20a6615bd7df2fc9b72d678cd0c325274
masahosono <dev.masahosono@gmail.com>
2020-07-19 11:44:13 +0900

    記事本文をdangerouslySetInnerHTMLで出力

--- src/component/Posts/index.tsx
+++ src/component/Posts/index.tsx
@@ -1,9 +1,22 @@
-import React from "react";
-
+import styled from "styled-components";
+import { ArticleData } from "types/article";
 interface Props {
-    text?: string
+    article: ArticleData
 }
 
-export default (props: Props) => {
-    return <div>{props.text}</div>;
-};
+const Article = styled.div`
+  h1 {
+    font-size: 30px
+  };
+  img {
+    max-width: 100%
+  }
+`;
+
+const Post = (props: Props) => (
+    <Article>
+        <span dangerouslySetInnerHTML={{ __html: props.article.text }} />
+    </Article>
+);
+
+export default Post;
